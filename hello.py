22300156fbefda0a87986c52c654f1af59a6b5fb
cmis <cmis@cmis-hs12.(none)>
2016-01-27 11:13:29 +0700

    edited the file

---
+++ hello.py
@@ -0,0 +1,11 @@
+print "Hello World"
+print "I'm Beth Jung"
+print "Nice to meet you"
+print True
+print False
+print 1234567890
+print 1.2
+print "Math", "is not hard", "you can do it"
+print "Candy", "Give me", "Yummy"
+print ("SaeBin", "Jung", 18)
+print ("Animal", "Dog", "Pomeranian")
