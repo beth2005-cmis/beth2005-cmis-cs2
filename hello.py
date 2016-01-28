8675de4ce4bd839195adf6370379426273703486
cmis <cmis@cmis-hs12.(none)>
2016-01-28 14:34:12 +0700

    edited the file

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
 print "Hello World"
 print "I'm Beth Jung"
 print "Nice to meet you"
+print "I want to go home"
+print "I miss mommy"
 print True
 print False
 print 1234567890
@@ -9,3 +11,11 @@ print "Math", "is not hard", "you can do it"
 print "Candy", "Give me", "Yummy"
 print ("SaeBin", "Jung", 18)
 print ("Animal", "Dog", "Pomeranian")
+myName = "Beth Jung"
+myAge = 18
+myGrade = 10
+print myName
+print myAge
+print myGrade
+print myName, myAge, myGrade
+print "HAHAHAHAHAHAHA"
