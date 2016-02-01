750c97a1c964ca1025056336a6d4867c7672eeaa
cmis <cmis@cmis-hs12.(none)>
2016-02-01 09:55:50 +0700

    Working on the assignment

---
+++ assignment1.py
@@ -0,0 +1,16 @@
+myName= "Beth Jung"
+myAge= 16.9
+myHeight= 1.66
+lSquare= 1
+lRectangle= 2
+hRectangle= 1
+ageInMonths= myAge * 12
+yearsToLive= myAge + ageInMonths
+heightInFeet= myHeight * 3.28
+differenceInHeight= myHeight - 1.6
+sArea= lSquare * lSquare
+hcVolume= 0.5 * sArea * lSquare
+nrVolume= 1.11 * lRectangle * hRectangle
+print "Hello. My name is " + myName + "."
+print "I am " + str(myHeight) + " meters tall."
+print "I am " + myAge + "years old so I have apporximately " + yearsToLive + "years remaining in my life..."
