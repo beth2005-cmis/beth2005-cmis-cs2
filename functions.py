3b33eb07d0f07838415690284fb0b53972d76cfd
cmis <cmis@cmis-hs12.(none)>
2016-02-05 09:55:42 +0700

    edited the file

---
+++ functions.py
@@ -0,0 +1,19 @@
+pi = 3.141592653589793
+print "pi: ", round(pi, 2)
+
+def chicken_wings():
+	print "Hey chicken wing"
+	print "I want to eat you"
+	print "Why are you so tasty?"
+	print "I'm so hungry"
+chicken_wings()
+
+def chicken_real(yummy):
+    print "Chicken is " + yummy
+
+chicken_real("delicious")
+chicken_real("crunchy")
+chicken_real("chewy")
+
+def numbers(a, b, c):
+    print b, c, a
