cf044d4f3ae8ac9295c70334d7885bb696abf9b1
cmis <cmis@cmis-hs12.(none)>
2016-02-09 14:06:21 +0700

    edited my assignment

--- functions.py
+++ functions.py
@@ -1,19 +1,37 @@
-pi = 3.141592653589793
-print "pi: ", round(pi, 2)
+#add two arguments
+def add(a, b):
+    return a + b
+c= add(3, 4)
+print c
+#subtract two arguments
+def sub(a, b):
+    return a - b
+c= sub(5, 3)
+print c
+#multiply two arguments
+def mul(a, b):
+    return a * b
+c= mul(4, 4)
+print c
+#divide two arguments
+def div(a, b):
+    return float(a)/b
+c= div(2, 3)
+print c
+#number of seconds to number of hours
+def hours_from_seconds(a, b):
+    return a / b
+c= div(86400, 3600)
+print c
+#one argument that represents the radius of a circle
+def circle_area(a):
+    return float(a)**2 *pi
+pi= 3.141592653589793
+b= 5**2
+c= b * pi
+print c
+#one argument that represents the radius of a sphere
+def sphere_volume(a):
+    return float(a)**3 *pi *4 /3
+pi= 3.141592653589793
 
-def chicken_wings():
-	print "Hey chicken wing"
-	print "I want to eat you"
-	print "Why are you so tasty?"
-	print "I'm so hungry"
-chicken_wings()
-
-def chicken_real(yummy):
-    print "Chicken is " + yummy
-
-chicken_real("delicious")
-chicken_real("crunchy")
-chicken_real("chewy")
-
-def numbers(a, b, c):
-    print b, c, a
