70f1e3a17f99f6428b8a8b01c21cf26425f7f92d
cmis <cmis@cmis-hs12.(none)>
2016-02-11 09:58:26 +0700

    edited the assignment

--- functions.py
+++ functions.py
@@ -1,3 +1,4 @@
+import math
 #add two arguments
 def add(a, b):
     return a + b
@@ -25,13 +26,25 @@ c= div(86400, 3600)
 print c
 #one argument that represents the radius of a circle
 def circle_area(a):
-    return float(a)**2 *pi
-pi= 3.141592653589793
-b= 5**2
-c= b * pi
+    return float(a)**2 * math.pi
+c= 5**2 * math.pi
 print c
 #one argument that represents the radius of a sphere
 def sphere_volume(a):
-    return float(a)**3 *pi *4 /3
-pi= 3.141592653589793
-
+    return (float(a)**3 * math.pi *4)/3
+c= (5**3 * math.pi * 4)/3
+print c
+#two integer or float arguments that represent the diameters of a sphere
+def avg_volume(a, b):
+    return ((((((float(a))/2)**3)*math.pi*4)/3)+(((((float(b))/2)**3)*math.pi*4)/3))/2
+c= ((((((10)/2)**3)*math.pi*4)/3)+(((((20)/2)**3)*math.pi*4)/3))/2
+print c
+#three side lengths of a triangle as arguments
+def area(a, b, c):
+    return math.sqrt((((a+b+c)/2))*(((a+b+c)/2)-a)*(((a+b+c)/2)-b)*(((a+b+c)/2)-c))
+c= math.sqrt((((1+2+2.5)/2))*(((1+2+2.5)/2)-1)*(((1+2+2.5)/2)-2)*(((1+2+2.5)/2)-2.5))
+print c
+print area(1,2,2.5)
+#string as an argument and perfectly right aligned
+def right_align(word):
+    return ((80-len(word))*" "+word
