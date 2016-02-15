245f842386dba8da12dc36833219b60137013df6
cmis <cmis@cmis-hs12.(none)>
2016-02-15 14:38:53 +0700

    created a file

---
+++ simpleprogram.py
@@ -0,0 +1,11 @@
+x= raw_input("type a number: ")
+y= raw_input("type another: ")
+z= int(x) + int(y)
+print str(x) + "+" + str(y) + "=" + str(z)
+
+prompt= 'What...is the airspeed velocity of an unladen swallow?\n'
+speed= raw_input(prompt)
+
+name= raw_input('What...is your name?\n')
+print name
+
