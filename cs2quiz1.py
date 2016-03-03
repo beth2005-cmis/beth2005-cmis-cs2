600c0015aaa96531b8df91334b03c20270278bc8
cmis <cmis@cmis-hs09.(none)>
2016-03-03 11:52:49 +0700

    checked quiz

--- cs2quiz1.py
+++ cs2quiz1.py
@@ -21,15 +21,15 @@
 #	4: string ('str')
 #   ex) "hello, world", "python"
 #	5: tuple ('tuple')
-#   ex) ("hi", "hello", 22, "hehe")
+#   ex) ("hi", "hello", 22, "hehe"), ("wassup", 15, "yo", "yoyo")
 #5 2pts) What is the difference between a "function definition" and a 
 #        "function call"?
 # A function definition is a statement that creates a new function, specifying its name, parameters, and the statements it executes. A function call is a statement that executes a function, and it consists of the funciton name followed by an argument list. 
 #
 #6 3pts) What are the 3 phases that every computer program has? What happens in each of them
-#	1:
-#	2:
-#	3:
+#	1: input -information supplied to the computer
+#	2: processing -performing input 
+#	3: output -infromation provided by the computer
 #
 #Part 2: Programming (25 points)
 #Write a program that asks the user for the areas of 3 circles.
@@ -45,14 +45,24 @@
 
 # Hint: Radius is the square root of the area divided by pi
 
+def radiusFromArea(area):
+    radius = math.sqrt(float(area)/math.pi)
+    return radius
+
 def main():  
-    prompt = 'what are the areas of 3 circles?\n' 
-#Circle  Diameter
-    c1 = raw_input("type a number: ")
-    c2 = raw_input("type second number: ")
-    c3 = raw_input("type third number: ")
-    totals = int(c1) + int(c2) + int(c3)
-    print prompt + totals
+#input
+    c1 = float(raw_input("area of c1: "))
+    c2 = float(raw_input("area of c2: "))
+    c3 = float(raw_input("area of c3: "))
+#processing
+    r1 = radius_from_Area(c1)
+    r2 = radius_from_Area(c2)
+    r3 = radius_from_Area(c3)
+
+    d1 = r1*2
+    d2 = r2*2
+    d3 = r3*2    
+    sumDiameters = d1 + d2 + d3
 
 import math
 def circle_area(radius1, radius2, radius3):
@@ -67,3 +77,6 @@ def sum_of_diameters(diameter1, diameter2, diameter3):
     c3 = diameter3/2
     total = c1 + c2 + c3
     return total
+#output
+
+main()
