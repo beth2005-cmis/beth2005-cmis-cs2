c857fa1833afb8ae198eebefafa649b86383bd66
cmis <cmis@cmis-hs12.(none)>
2016-03-15 09:55:57 +0700

    assignment

--- oneguess.py
+++ oneguess.py
@@ -8,4 +8,11 @@ The target was 9.
 Your guess was 7.
 That's under by 2.
 
-def
+input random
+
+def any_number():
+    number = int(raw_input("Guess the number (5-10):")
+    if number > 10 or number < 5:
+        number = random.randint(5, 10)
+    any_number = float(number)
+    return any_number
