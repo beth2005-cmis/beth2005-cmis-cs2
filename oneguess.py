dbb93356e5f5841c60241320d237cbafc25e68a6
cmis <cmis@cmis-hs12.(none)>
2016-03-15 09:40:35 +0700

    conditional execution

---
+++ oneguess.py
@@ -0,0 +1,11 @@
+Sample Output:
+What is the minimum number? 5
+What is the maximum number? 10
+I'm thinking of a number from 5 to 10.
+What do you think it is?: 7
+
+The target was 9.
+Your guess was 7.
+That's under by 2.
+
+def
