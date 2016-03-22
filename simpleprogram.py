af26f8747342c159b78b7e88c9690bf2e656475d
cmis <cmis@cmis-hs05.(none)>
2016-03-22 09:57:22 +0700

    edited

--- simpleprogram.py
+++ simpleprogram.py
@@ -1,41 +1,25 @@
-x= raw_input("type a number: ")
-y= raw_input("type another: ")
-z= int(x) + int(y)
-print str(x) + "+" + str(y) + "=" + str(z)
-
-prompt= 'What...is the airspeed velocity of an unladen swallow?\n'
-speed= raw_input(prompt)
-name= raw_input('What...is your name?\n')
-print "Hey " + name + "!"
-
-output= """
-Hey {}
-Did you know:
-{} + {} = {}
-{}
-{}
-""". format(name, x,y,z)
-print output
-
 import math
-def qdr(a, b, c):
-    return ((-b+math.sqrt(b**2-4*a*c))/2*a)
 
-print output
+def qdr(a, b, c):
+	d = abs((b**2) - (4*a*c))
+    x= (-b+math.sqrt(d))/(2*a)
+    return x
+
 def output(name, a, b, c, d):
     out= """
 Hey {}
 Did you know:
 ({}+math.sqrt{})/{})
-""". format(name, a,b,c,d)
+""". format(name, a, b, c, d)
+    return out
 
 def main():
     name= raw_input("What's your name?:")
-    a= raw_input("Favorite number1:")
-    b= raw_input("Favorite number2:")
-    c= raw_input("Favorite number3:")
-    d= (int(-b)+math.sqrt(int(b)**2-4*int(a)*int(c))/2*int(a)
-    out= output (name, a, b, c, d)
+    a= float(raw_input("Favorite number1:"))
+    b= float(raw_input("Favorite number2:"))
+    c= float(raw_input("Favorite number3:"))
+    d= qdr(a, b, c)
+    out= output(name, a, b, c, d)
     print out
 
 main()
