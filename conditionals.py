9aa1b3a192869403a1b3e0704a8a1f00630fc679
cmis <cmis@cmis-hs05.(none)>
2016-03-23 11:55:16 +0700

    assignments

---
+++ conditionals.py
@@ -0,0 +1,6 @@
+#My script will be a pokemon game. You are walking in the forest on your way home and meet a wild pokemon. You have no way out unless you fight with the wild pokemon that is blocking your way. You have Pikachu as your fighter against the pokemon, and it only has  three skills to attack. The wild pokemon has an HP of 100,and a skill that takes away 15HP of the opponent. Pikachu also has 100HP, and an attack skill, "scratch", takes away 8HP, "bolt" takes away 9HP, lastly "poke" takes away 7HP. You have two potions that provides Pikachu with +30HP. 
+
+import math
+import random
+
+def 
--- oneguess.py
+++ oneguess.py
@@ -1,18 +1,51 @@
-Sample Output:
-What is the minimum number? 5
-What is the maximum number? 10
-I'm thinking of a number from 5 to 10.
-What do you think it is?: 7
+import random
+import math
 
-The target was 9.
-Your guess was 7.
-That's under by 2.
+def sub(a, b):
+	return int(a) - int(b)
+def add(c, d):
+	return int(c) + int(d)
 
-input random
+def guessing_small(chosen_number, estimation, difference):
+	guess_small = """
+The target was {}.
+Your guess was {}.
+That's under by {}.
+""".format(chosen_number, estimation, difference)
+	return guess_small
 
-def any_number():
-    number = int(raw_input("Guess the number (5-10):")
-    if number > 10 or number < 5:
-        number = random.randint(5, 10)
-    any_number = float(number)
-    return any_number
+def guessing_big(chosen_number, estimation, difference):
+	guess_big = """
+The target was {}.
+Your guess was {}.
+That's over by {}.
+""".format(chosen_number, estimation, difference)
+	return guess_big
+
+def guessing_correct(chosen_number, estimation):
+	guess_correct = """
+The target was {}.
+Your guess was {}.
+That's correct.
+""".format(chosen_number, estimation)
+	return guess_correct
+
+def main():
+	#Input
+	minimum = int(raw_input("What is the minimum number? "))
+	maximum = int(raw_input("What is the maximum number? "))
+	estimation = raw_input("I'm thinking of a number between " + str(minimum) + " and " + str(maximum) + ".\n" + "What do you think it is?: ")
+	#Processing
+	chosen_number = int(random.randint(minimum, maximum))
+	difference = abs(sub(int(estimation), chosen_number))	
+	#Output
+	if chosen_number < abs(int(estimation)):	
+		guess_small = guessing_small(chosen_number, estimation, difference)
+		print guess_small
+	elif chosen_number > abs(int(estimation)):
+		guess_big = guessing_big(chosen_number, estimation, difference)
+		print guess_big
+	elif chosen_number == abs(int(estimation)):
+		guess_correct = guessing_correct(chosen_number, estimation)
+		print guess_correct
+main()
