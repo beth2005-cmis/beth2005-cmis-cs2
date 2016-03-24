df6d08a796ba62e0ea8b4975cfc5127738d6e78e
cmis <cmis@cmis-hs00.(none)>
2016-03-24 14:06:12 +0700

    edited

--- conditionals.py
+++ conditionals.py
@@ -1,6 +1,44 @@
-#My script will be a pokemon game. You are walking in the forest on your way home and meet a wild pokemon. You have no way out unless you fight with the wild pokemon that is blocking your way. You have Pikachu as your fighter against the pokemon, and it only has  three skills to attack. The wild pokemon has an HP of 100,and a skill that takes away 15HP of the opponent. Pikachu also has 100HP, and an attack skill, "scratch", takes away 8HP, "bolt" takes away 9HP, lastly "poke" takes away 7HP. You have two potions that provides Pikachu with +30HP. 
+#My scrip is a math game. You are given five questions in four different stages. For each stages, if you miss more than three questions, then the game will restart for you. You only have 2 minutes to answer these questions. Your goal is to finish the last stage and happily go home. 
 
 import math
 import random
+import time
 
-def 
+#Scores for each Stages
+correct = 0
+incorrect = 0
+
+#List of question in Stage 1
+def addition(
+
+#Instructions before the game starts
+print("You have 20 seconds to finish Stage 1.")
+input("Hit 'enter' to begin")
+
+#start the timer
+start = time.time()
+print("The clock has started!!")
+
+#List of questions
+for q in questions:
+    #display question and get answer
+    answer = response(q + ": ", ['t','f']) == 't'
+    result = eval(q)
+    #compare answer to actual result
+    if answer == result:
+        print("That's right!")
+        correct += 1
+    else:
+        print("That's wrong!")
+        incorrect += 1
+    print(q + " is " + str(result) + "\n\n")
+end = time.time()
+
+#Results
+print("Your time was: ", end - start, " seconds")
+print("You got ", correct, " correct.")
+print("You got ", incorrect, " incorrect.")
+if incorrect > 0:
+    print("Restart")
+else:
+    print("You passed, so go HOME!")
