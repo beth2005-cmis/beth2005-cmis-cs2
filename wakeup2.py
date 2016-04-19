aaca06a3f0cd9feb45fad19ac1dc1417720217c9
cmis <cmis@cmis-hs15.(none)>
2016-04-19 11:40:49 +0700

    finished

--- wakeup2.py
+++ wakeup2.py
@@ -8,39 +8,49 @@ It will print out whether the resulting average is even or odd.
 """
 
 def main():
-    amount = 0    
+    amount = 0
+    total = 0 
+    avg_n = 0   
     #input
     n0= float(raw_input("n0 "))
     n1= float(raw_input("n1 "))
     n2= float(raw_input("n2 "))
     n3= float(raw_input("n3 "))
     n4= float(raw_input("n4 "))
-    #processing
-    average = (n0 + n1 + n2 + n3 + n4)/5
+    
     #output
     if n0 > 0 and n0 < 10:
-        total = amount += 1
+        avg_n = n0
+        total += 1
     else:
-        total = amount
-        print n0, " is out of range."
+        print n0, "is out of range."
+
     if n1 > 0 and n1 < 10:
-        total = amount += 1
+        avg_n = avg_n + n1
+        total += 1
     else:
-        total = amount
-        print n1, " is out of range."
+        print n1, "is out of range."
+
     if n2 > 0 and n2 < 10:
-        total = amount += 1
+        total += 1
+        avg_n = avg_n + n2
     else:
-        total = amount
-        print n2, " is out of range."
+        print n2, "is out of range."
+
     if n3 > 0 and n3 < 10:
-        total = amount += 1
+        total += 1
+        avg_n = avg_n + n3
     else:
-        total = amount
-        print n3, " is out of range."
+        print n3, "is out of range."
+
     if n4 > 0 and n4 < 10:
-        total = amount += 1
+        total += 1
+        avg_n = avg_n + n4
     else:
-        total = amount
-        print n4, " is out of range."
+        print n4, "is out of range."
+
+    average = (avg_n)/total
+    print average
+
+main()
 
