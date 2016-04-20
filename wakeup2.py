fc9c33175269198f905e329e3c1253c3c250160e
cmis <cmis@cmis-hs15.(none)>
2016-04-20 13:48:10 +0700

    finished assignment

--- wakeup2.py
+++ wakeup2.py
@@ -19,31 +19,31 @@ def main():
     n4= float(raw_input("n4 "))
     
     #output
-    if n0 > 0 and n0 < 10:
+    if n0 >= 0 and n0 < 10:
         avg_n = n0
         total += 1
     else:
         print n0, "is out of range."
 
-    if n1 > 0 and n1 < 10:
+    if n1 >= 0 and n1 < 10:
         avg_n = avg_n + n1
         total += 1
     else:
         print n1, "is out of range."
 
-    if n2 > 0 and n2 < 10:
+    if n2 >= 0 and n2 < 10:
         total += 1
         avg_n = avg_n + n2
     else:
         print n2, "is out of range."
 
-    if n3 > 0 and n3 < 10:
+    if n3 >= 0 and n3 < 10:
         total += 1
         avg_n = avg_n + n3
     else:
         print n3, "is out of range."
 
-    if n4 > 0 and n4 < 10:
+    if n4 >= 0 and n4 < 10:
         total += 1
         avg_n = avg_n + n4
     else:
