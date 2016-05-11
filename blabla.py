4f4dac870d8f095eba715c44f544899418e1206a
cmis <cmis@cmis-hs05.(none)>
2016-05-11 09:13:01 +0700

    aaa

---
+++ blabla.py
@@ -0,0 +1,5 @@
+def addition_questions():
+    x = random.randint(1, 10)
+    y = random.rantint(1, 10)
+    return x + y
+print addition_questions
--- conditionals_Final.py
+++ conditionals_Final.py
@@ -14,5 +14,24 @@ incorrect = 0
 def addition_questions():
     x = random.randint(1, 10)
     y = random.rantint(1, 10)
-    return int('x + y')
+    return x + y
 print addition_questions
+
+#compare answer to actual result
+if answer == result:
+	print("That's right!")
+    correct += 1
+else:
+    print("That's wrong!")
+    incorrect += 1
+print(q + " is " + str(result) + "\n\n")
+
+print("Your time was: ", end - start, " seconds")
+print("You got ", correct, " correct.")
+print("You got ", incorrect, " incorrect.")
+if incorrect > 2:
+    print("Restart")
+else:
+    print("You passed!")
+if (end - start) > 20
+    input("You're funny...Do it again!")
