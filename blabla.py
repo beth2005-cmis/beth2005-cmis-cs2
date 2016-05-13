b676a600e6a5d19c7211e6d2d1b9ed8c4d5787e5
cmis <cmis@cmis-hs05.(none)>
2016-05-13 14:36:32 +0700

    ish

--- blabla.py
+++ blabla.py
@@ -4,110 +4,49 @@ import math
 correct = 0
 incorrect = 0
 
-def addition_question1(x, y):
+
+def addition_question(x, y, times, correct, incorrect):
     x = random.randint(1, 10)
     y = random.randint(1, 10)
-
     answer = raw_input(str(x) + "+" + str(y))
-    actual_answer = x + y
-    if answer == actual_answer:
+    if times == 0:
+        print "you got ", correct, " correct."
+        print "You got ", incorrect, " incorrect."
+    elif int(answer) == x + y:
         print("That's right!")
         correct += 1
-        return addition_question1(x, y)
-    elif 
+        addition_question(x, y, times - 1, correct, incorrect)
+    else: 
         print("That's wrong!")
-        incorrect += 1
-        addition_question1(x, y)
+        incorrect = incorrect + 1
+        addition_question(x, y, times - 1, correct, incorrect)
+
+def subtraction_question(x, y, times, correct, incorrect):
+    x = random.randint(1, 10)
+    y = random.randint(1, 10)
+    answer = raw_input(str(x) + "-" + str(y))
+    if times == 0:
+        print "you got ", correct, " correct."
+        print "You got ", incorrect, " incorrect."
+    elif int(answer) == x - y:
+        print("That's right!")
+        correct += 1
+        subtraction_question(x, y, times - 1, correct, incorrect)
+    else: 
+        print("That's wrong!")
+        incorrect = incorrect + 1
+        subtraction_question(x, y, times - 1, correct, incorrect)
+
 
 def main():
     x = random.randint(1, 10)
     y = random.randint(1, 10)
-    addition_question1(x, y)
+    times = 5
+    correct = 0
+    incorrect = 0
+    addition_question(x, y, times, correct, incorrect)
+    subtraction_question(x, y, times, correct, incorrect)
 main()
 
-def addition_question2():
-    x = random.randint(1, 10)
-    y = random.randint(1, 10)
-    print raw_input(str(x) + "+" + str(y))
-    answer2 = raw_input(str(x) + "+" + str(y))
-    actual_answer2 = x + y
-    if answer2 == actual_answer2:
-        print("That's right!")
-        correct += 1
-    else:
-        print("That's wrong!")
-        incorrect += 1
-        addition_question2()
 
-def main():
-    x = random.randint(1, 10)
-    y = random.randint(1, 10)
-    addition_question1(x, y)
-main()
 
-def addition_question3():
-    x = random.randint(1, 10)
-    y = random.randint(1, 10)
-    print raw_input(str(x) + "+" + str(y))
-    answer3 = raw_input(str(x) + "+" + str(y))
-    actual_answer3 = x + y
-    if answer3 == actual_answer3:
-        print("That's right!")
-        correct += 1
-    else:
-        print("That's wrong!")
-        incorrect += 1
-        addition_question3()
-
-def main():
-    x = random.randint(1, 10)
-    y = random.randint(1, 10)
-    addition_question1(x, y)
-main()
-
-def addition_question4():
-    x = random.randint(1, 10)
-    y = random.randint(1, 10)
-    print raw_input(str(x) + "+" + str(y))
-    answer4 = raw_input(str(x) + "+" + str(y))
-    actual_answer4 = x + y
-    if answer4 == actual_answer4:
-        print("That's right!")
-        correct += 1
-    else:
-        print("That's wrong!")
-        incorrect += 1
-        addition_question4()
-
-def main():
-    x = random.randint(1, 10)
-    y = random.randint(1, 10)
-    addition_question1(x, y)
-main()
-
-def addition_question5():
-    x = random.randint(1, 10)
-    y = random.randint(1, 10)
-    print raw_input(str(x) + "+" + str(y))
-    answer5 = raw_input(str(x) + "+" + str(y))
-    actual_answer5 = x + y
-    if answer5 == actual_answer5:
-        print("That's right!")
-        correct += 1
-    else:
-        print("That's wrong!")
-        incorrect += 1
-        addition_question5()
-
-def main():
-    x = random.randint(1, 10)
-    y = random.randint(1, 10)
-    addition_question1(x, y)
-main()
-
-print "You got ", correct, " correct."
-print "You got ", incorrect, " incorrect."
-if incorrect > 2:
-    print "game over"
-else:
-    print "You passed!"
