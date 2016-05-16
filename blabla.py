cc3997bdfe52ea7669e92267cccb980b446afd51
cmis <cmis@cmis-hs12.(none)>
2016-05-16 14:37:44 +0700

    right

--- blabla.py
+++ blabla.py
@@ -20,6 +20,8 @@ def addition_question(x, y, times, correct, incorrect):
         print("That's wrong!")
         incorrect = incorrect + 1
         addition_question(x, y, times - 1, correct, incorrect)
+    correct = a_correct
+    incorrect = a_incorrect
 
 def subtraction_question(x, y, times, correct, incorrect):
     x = random.randint(1, 10)
@@ -36,7 +38,44 @@ def subtraction_question(x, y, times, correct, incorrect):
         print("That's wrong!")
         incorrect = incorrect + 1
         subtraction_question(x, y, times - 1, correct, incorrect)
+    correct = s_correct
+    incorrect = s_incorrect
 
+def division_question(x, y, times, correct, incorrect):
+    x = random.randint(1, 10)
+    y = random.randint(1, 10)
+    answer = raw_input(str(x) + "/" + str(y))
+    if times == 0:
+        print "you got ", correct, " correct."
+        print "You got ", incorrect, " incorrect."
+    elif float(answer) == x / y:
+        print("That's right!")
+        correct += 1
+        division_question(x, y, times - 1, correct, incorrect)
+    else: 
+        print("That's wrong!")
+        incorrect = incorrect + 1
+        division_question(x, y, times - 1, correct, incorrect)
+    correct = d_correct
+    incorrect = d_incorrect
+
+def multi_question(x, y, times, correct, incorrect):
+    x = random.randint(1, 10)
+    y = random.randint(1, 10)
+    answer = raw_input(str(x) + "*" + str(y))
+    if times == 0:
+        print "you got ", correct, " correct."
+        print "You got ", incorrect, " incorrect."
+    elif int(answer) == x * y:
+        print("That's right!")
+        correct += 1
+        multi_question(x, y, times - 1, correct, incorrect)
+    else: 
+        print("That's wrong!")
+        incorrect = incorrect + 1
+        multi_question(x, y, times - 1, correct, incorrect)
+    correct = m_correct 
+    incorrect = m_incorrect
 
 def main():
     x = random.randint(1, 10)
@@ -46,7 +85,17 @@ def main():
     incorrect = 0
     addition_question(x, y, times, correct, incorrect)
     subtraction_question(x, y, times, correct, incorrect)
-main()
+    division_question(x, y, times, correct, incorrect)
+    multi_question(x, y, times, correct, incorrect)
 
+    total_c = a_correct + s_correct + d_correct + m_correct
+    total_i = a_incorrect + s_incorrect + d_incorrect + m_incorrect 
+    print "You got a total number of ", total_c, " correct."
+    print "\nYou got a total number of ", total_i, " incorrect."
+    if total_c <= total_i:
+        print "DON'T HATE MATH! MATH LIKES YOU"
+    else:
+        print "YOU HAVE NO LIFE BUT YOU'RE DOIND GREAT X)"
+main()
 
 
--- conditionals_Final.py
+++ conditionals_Final.py
@@ -1,4 +1,4 @@
-#My scrip is a math game. You are given five questions in four different stages. For each stages, if you miss more than three questions, then the game will restart for you. Your goal is to finish the last stage and happily go home. 
+#My scrip is a math game. You are given five questions in four different stages. Your goal is to finish the given task. 
 
 import math
 import random
@@ -7,82 +7,106 @@ correct = 0
 incorrect = 0
 
 #Instructions before the game starts
+print "You are given five questions in four different stages. For the sixth questions in each stages, press 'enter' to continue."
 input("Hit 'enter' to begin")
 
-def addition_question1():
+import random
+import math
 
+correct = 0
+incorrect = 0
+
+
+def addition_question(x, y, times, correct, incorrect):
     x = random.randint(1, 10)
     y = random.randint(1, 10)
-    actual_answer = x + y
-    print raw_input(str(x) + "+" + str(y))
     answer = raw_input(str(x) + "+" + str(y))
-    if answer == actual_answer:
+    if times == 0:
+        print "you got ", correct, " correct."
+        print "You got ", incorrect, " incorrect."
+    elif int(answer) == x + y:
         print("That's right!")
         correct += 1
-    else:
+        addition_question(x, y, times - 1, correct, incorrect)
+    else: 
         print("That's wrong!")
-        incorrect += 1
-addition_question1()
+        incorrect = incorrect + 1
+        addition_question(x, y, times - 1, correct, incorrect)
+    correct = a_correct
+    incorrect = a_incorrect
 
-def addition_question2():
+def subtraction_question(x, y, times, correct, incorrect):
     x = random.randint(1, 10)
     y = random.randint(1, 10)
-    print raw_input(str(x) + "+" + str(y))
-    answer2 = raw_input(str(x) + "+" + str(y))
-    actual_answer2 = x + y
-    if answer2 == actual_answer2:
+    answer = raw_input(str(x) + "-" + str(y))
+    if times == 0:
+        print "you got ", correct, " correct."
+        print "You got ", incorrect, " incorrect."
+    elif int(answer) == x - y:
         print("That's right!")
         correct += 1
-    else:
+        subtraction_question(x, y, times - 1, correct, incorrect)
+    else: 
         print("That's wrong!")
-        incorrect += 1
-addition_question2()
+        incorrect = incorrect + 1
+        subtraction_question(x, y, times - 1, correct, incorrect)
+    correct = s_correct
+    incorrect = s_incorrect
 
-def addition_question3():
+def division_question(x, y, times, correct, incorrect):
     x = random.randint(1, 10)
     y = random.randint(1, 10)
-    print raw_input(str(x) + "+" + str(y))
-    answer3 = raw_input(str(x) + "+" + str(y))
-    actual_answer3 = x + y
-    if answer3 == actual_answer3:
+    answer = raw_input(str(x) + "/" + str(y))
+    if times == 0:
+        print "you got ", correct, " correct."
+        print "You got ", incorrect, " incorrect."
+    elif float(answer) == x / y:
         print("That's right!")
         correct += 1
-    else:
+        division_question(x, y, times - 1, correct, incorrect)
+    else: 
         print("That's wrong!")
-        incorrect += 1
-addition_question3()
+        incorrect = incorrect + 1
+        division_question(x, y, times - 1, correct, incorrect)
+    correct = d_correct
+    incorrect = d_incorrect
 
-def addition_question4():
+def multi_question(x, y, times, correct, incorrect):
     x = random.randint(1, 10)
     y = random.randint(1, 10)
-    print raw_input(str(x) + "+" + str(y))
-    answer4 = raw_input(str(x) + "+" + str(y))
-    actual_answer4 = x + y
-    if answer4 == actual_answer4:
+    answer = raw_input(str(x) + "*" + str(y))
+    if times == 0:
+        print "you got ", correct, " correct."
+        print "You got ", incorrect, " incorrect."
+    elif int(answer) == x * y:
         print("That's right!")
         correct += 1
-    else:
+        multi_question(x, y, times - 1, correct, incorrect)
+    else: 
         print("That's wrong!")
-        incorrect += 1
-addition_question4()
+        incorrect = incorrect + 1
+        multi_question(x, y, times - 1, correct, incorrect)
+    correct = m_correct 
+    incorrect = m_incorrect
 
-def addition_question5():
+def main():
     x = random.randint(1, 10)
     y = random.randint(1, 10)
-    print raw_input(str(x) + "+" + str(y))
-    answer5 = raw_input(str(x) + "+" + str(y))
-    actual_answer5 = x + y
-    if answer5 == actual_answer5:
-        print("That's right!")
-        correct += 1
-    else:
-        print("That's wrong!")
-        incorrect += 1
-addition_question5()
+    times = 5
+    correct = 0
+    incorrect = 0
+    addition_question(x, y, times, correct, incorrect)
+    subtraction_question(x, y, times, correct, incorrect)
+    division_question(x, y, times, correct, incorrect)
+    multi_question(x, y, times, correct, incorrect)
 
-print "You got ", correct, " correct."
-print "You got ", incorrect, " incorrect."
-if incorrect > 2:
-    print "game over"
-else:
-    print "You passed!"
+    total_c = a_correct + s_correct + d_correct + m_correct
+    total_i = a_incorrect + s_incorrect + d_incorrect + m_incorrect 
+    print "You got a total number of ", total_c, " correct."
+    print "\nYou got a total number of ", total_i, " incorrect."
+    if total_c <= total_i:
+        print "DON'T HATE MATH! MATH LIKES YOU"
+    else:
+        print "YOU HAVE NO LIFE BUT YOU'RE DOIND GREAT X)"
+main()
+
