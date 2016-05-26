2e9aa8853fad74877466873d1484d55b9b966069
cmis <cmis@cmis-hs17.(none)>
2016-05-26 14:36:13 +0700

    blabla

--- conditionals_Final.py
+++ conditionals_Final.py
@@ -7,7 +7,7 @@ correct = 0
 incorrect = 0
 
 #Instructions before the game starts
-print "You are given five questions in three different stages. For the sixth questions in each stages, press 'enter' to continue. Start the game right away."
+print "You are given five questions in three different stages. Press 'enter' to ignore sixth question. Start the game right away."
 
 a_correct = 0
 a_incorrect = 0
@@ -22,8 +22,11 @@ def addition_question(x, y, times, a_correct, a_incorrect):
     y = random.randint(1, 10)
     answer = raw_input(str(x) + "+" + str(y))
     if times == 0:
-        print "you got ", a_correct, " correct."
-        print "You got ", a_incorrect, " incorrect."
+        out ="""
+You got {} correct.
+\nYou got {} incorrect.
+""".format(a_correct, a_incorrect)
+        print out
     elif int(answer) == x + y:
         print("That's right!")
         a_correct += 1
@@ -32,19 +35,24 @@ def addition_question(x, y, times, a_correct, a_incorrect):
         print("That's wrong!")
         a_incorrect = a_incorrect + 1
         addition_question(x, y, times - 1, a_correct, a_incorrect) 
-        return a_correct
+
 
 def subtraction_question(x, y, times, s_correct, s_incorrect):
     x = random.randint(1, 10)
     y = random.randint(1, 10)
     answer = raw_input(str(x) + "-" + str(y))
     if times == 0:
-        print "you got ", s_correct, " correct."
-        print "You got ", s_incorrect, " incorrect."
+        out ="""
+You got {} correct.
+\nYou got {} incorrect.
+""".format(s_correct, s_incorrect)
+        print out
     elif int(answer) == x - y:
         print("That's right!")
         s_correct += 1
         subtraction_question(x, y, times - 1, s_correct, s_incorrect)
+    elif answer == "":
+        print("That's wrong!")
     else: 
         print("That's wrong!")
         s_incorrect = s_incorrect + 1
@@ -55,8 +63,11 @@ def multi_question(x, y, times, m_correct, m_incorrect):
     y = random.randint(1, 10)
     answer = raw_input(str(x) + "*" + str(y))
     if times == 0:
-        print "you got ", m_correct, " correct."
-        print "You got ", m_incorrect, " incorrect."
+        out ="""
+You got {} correct.
+\nYou got {} incorrect.
+""".format(m_correct, m_incorrect)
+        print out
     elif int(answer) == int(x * y):
         print("That's right!")
         m_correct += 1
@@ -70,18 +81,8 @@ def main():
     x = random.randint(1, 10)
     y = random.randint(1, 10)
     times = 5
-
     addition_question(x, y, times, a_correct, a_incorrect)
     subtraction_question(x, y, times, s_correct, s_incorrect)
     multi_question(x, y, times, m_correct, m_incorrect)
-
-    total_c = a_correct + s_correct + m_correct
-    total_i = a_incorrect + s_incorrect + m_incorrect 
-    print "\nYou got a total number of ", total_c, " correct."
-    print "You got a total number of ", total_i, " incorrect."
-    if total_c <= total_i:
-        print " PLEASE DON'T HATE MATH! MATH LIKES YOU"
-    else:
-        print "YOU HAVE NO LIFE BUT YOU'RE DOIND GREAT X)"
 main()
 
--- whileloops.py
+++ whileloops.py
@@ -20,8 +20,29 @@
 #            x += 1
 #countup(-10)
 
-def addup(x):
-    while x >= 0:
-        print x + (x-1)
-        x -= 1
-addup(10)
+#def addup(x):
+#    while x >= 0:
+#        print x + (x-1)
+#        x -= 1
+#addup(10)
+
+#def countfrom(x, y):
+#    if x < y:
+#        while x <= y:
+#            print x
+#            x += 1
+#    else:
+#        while y <= x:
+#            print y
+#            y += 1
+#countfrom(-10,10)
+
+def sum_of_odds(n):
+    if n < 0:
+            while n <= 0:
+                print n
+                n += 1
+    else:
+        while n >= 0:
+            print n
+sum_of_odds(10)
